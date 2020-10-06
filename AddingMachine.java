d19ad50900e90c16019c6d7d8aa59e0c6773950c
gvipat-asu <gvipat@asu.edu>
2020-10-05 23:19:49 -0700

    Added adding machine class and documented methods

---
+++ AddingMachine.java
@@ -0,0 +1,64 @@
+//package cse360assignment02;
+
+/**
+* 360 Assignment 01 java example class Adding Machine
+*
+* @author  Gaurav Vipat
+* @version 1.0
+* @since   10.05.2020
+*/
+public class AddingMachine {
+
+  private int total; // int to track total
+  
+  /**
+   * Adding machine contrsuctor initializes total to 0
+   * @param none
+   * @return none
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * Accessor method for private total variable
+   * @param none
+   * @return total
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * Add method adds param to total
+   * @param value some integer
+   * @return none
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * Subtract method subtracts param from total
+   * @param value value to subtract
+   * @return none
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * To string method prinotus out total
+   * @param none
+   * @return none
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * Clear method resets total
+   * @param none
+   * @return none
+   */
+  public void clear() {
+  }
+}
